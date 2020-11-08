c7a7e703fbd7fa5c246329a2b11356d76b7e778b
sreeja99 <srijagodishala@gmail.com>
2020-11-08 23:35:47 +0530

    Added:Checking If Date Falls In Time Period

---
+++ PracticeProblems/SelectionPractice1.2.js
@@ -0,0 +1,10 @@
+let month = 4
+    let day = 25
+    if(day<=31&&month<=12){
+        if(day>20&&month>=3&&month<=6)
+            console.log("True");
+        else 
+            console.log("False");
+    }
+    else 
+        console.log("Invalid Date")
