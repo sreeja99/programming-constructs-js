a33994c844226405fbe0ecbdf244f35bd92e22de
sreeja99 <srijagodishala@gmail.com>
2020-11-08 23:07:55 +0530

    Added:Checking For Prime And Palindrome Subseqently

---
+++ PracticeProblems/Functions3.js
@@ -0,0 +1,38 @@
+function primeNumberChecker(num){
+    console.log("Number is: "+num);
+    if(num==1)
+        console.log("Not Prime");
+    else{
+        for(let i=2;i<=num;i++){
+            if(i==num){
+                console.log("Prime");
+                return true;     
+            }
+            if(num%i==0){
+                console.log("Not prime");
+                return false;
+            }
+        }
+    }
+}
+
+function getPalindrome(num){
+    let palindrome=0;
+    while(num>0){
+        let rem = num%10;
+        palindrome = palindrome*10 +rem;
+        num=Math.floor(num/10);
+    }
+    return palindrome;
+}
+
+
+let num = Math.ceil(Math.random()*100); 
+let isPrime = primeNumberChecker(num);
+if(isPrime==false)
+    console.log("Number is not prime so not checking palindrome");
+if(isPrime){
+    console.log("Number is prime. Checking palindrome")
+    let palindrome = getPalindrome(num);
+    primeNumberChecker(palindrome);
+}
