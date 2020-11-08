e2a0f158b56b18bb102fc3c5ef187aca15317363
sreeja99 <srijagodishala@gmail.com>
2020-11-08 22:46:02 +0530

    Added:Finding Factorial For Taken Input

---
+++ PracticeProblems/ForLoop5.js
@@ -0,0 +1,7 @@
+let num = Math.ceil(Math.random()*10); 
+console.log("Number = "+num);
+let factorial=1;
+for(let i=1;i<=num;i++){
+    factorial = factorial*i;
+}
+console.log("Factorial: "+factorial);
