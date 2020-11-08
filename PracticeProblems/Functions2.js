8b3c7b9d2a2adecbae01b956513c6777bd58c059
sreeja99 <srijagodishala@gmail.com>
2020-11-08 23:02:56 +0530

    Added:Checking Whether Two Numbers Are Palindomes

---
+++ PracticeProblems/Functions2.js
@@ -0,0 +1,18 @@
+function palindromeChecker(num1,num2){
+    let palindromeNum1=0;
+    while(num1>0){
+        let rem = num1%10;
+        palindromeNum1 = palindromeNum1*10 +rem;
+        num1=Math.floor(num1/10);
+    }
+    if(palindromeNum1 == num2)
+        return true;
+    else 
+        return false;
+}
+
+let num1 = 143;
+let num2 = 341;
+let isPalindrome = palindromeChecker(num1,num2);
+console.log("Numbers: "+num1+", "+num2);
+console.log("Is number 2 a palindrome of number 1: "+isPalindrome);
