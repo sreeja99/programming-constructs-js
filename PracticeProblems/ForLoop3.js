881c25b45c3a94c014caac93a0db71bd1d49d66c
sreeja99 <srijagodishala@gmail.com>
2020-11-08 22:36:23 +0530

    Added:Finding Whether A Number Is Prime

---
+++ PracticeProblems/ForLoop3.js
@@ -0,0 +1,16 @@
+let num = Math.ceil(Math.random()*100); 
+console.log("Number is: "+num);
+if(num==1)
+    console.log("Not Prime");
+else{
+    for(let i=2;i<=num;i++){
+        if(i==num){
+            console.log("Prime");
+            break;
+        }
+        if(num%i==0){
+            console.log("Not prime");
+            break;
+        }
+    }
+}
