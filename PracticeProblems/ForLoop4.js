db5b5b979b0a3c52e50e914e7fe576f3d27744da
sreeja99 <srijagodishala@gmail.com>
2020-11-08 22:43:01 +0530

    Added:finding Prime Numbers In Particluar Range

---
+++ PracticeProblems/ForLoop4.js
@@ -0,0 +1,19 @@
+let start = Math.ceil(Math.random()*10); 
+let end = Math.ceil(Math.random()*100);
+console.log("Start="+start);
+console.log("End="+end);
+if(end>start){
+    if(start==1)
+    console.log("Not Prime");
+    else{
+        for(let i=start;i<=end;i++){
+            for(let j=2;j<=i;j++){
+                if(j==i){
+                    console.log("Prime number: "+i);
+                }
+                if(i%j==0)
+                    break;   
+            }
+        }
+    }
+}
